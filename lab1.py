d6dc50e79aefd763001d88cb228335d8da96833a
254352 <228599@edu.p.lodz.pl>
2024-04-26 19:38:46 +0200

    Generowanie punktów płaszczyzny

--- lab1.py
+++ lab1.py
@@ -1,10 +1,11 @@
 from scipy.stats import norm
 from csv import writer
+import numpy as np
 
-def generate_points(num_points:int=2000):
-    distribution_x = norm(loc=0, scale=28)
+def generate_points_A(num_points:int=2000):
+    distribution_x = norm(loc=0, scale=50)
     distribution_y = norm(loc=0, scale=200)
-    distribution_z = norm(loc=0.2, scale=0.05)
+    distribution_z = norm(loc=0, scale=0)
 
     x = distribution_x.rvs(size=num_points)
     y = distribution_y.rvs(size=num_points)
@@ -13,11 +14,53 @@ def generate_points(num_points:int=2000):
     points = zip(x,y,z) #tworzy listę krotek x,y,z
     return points
 
-cloud_points = generate_points(2000)
+def generate_points_B(num_points:int=2000):
+    distribution_x = norm(loc=0, scale=0)
+    distribution_y = norm(loc=0, scale=50)
+    distribution_z = norm(loc=0, scale=200)
 
-with open('LidarData.xyz', 'w', encoding='utf-8', newline='\n') as csvfile:
+    x = distribution_x.rvs(size=num_points)
+    y = distribution_y.rvs(size=num_points)
+    z = distribution_z.rvs(size=num_points)
+
+    points = zip(x,y,z) #tworzy listę krotek x,y,z
+    return points
+
+def generate_points_C(num_points:int=2000, r:int=10):
+    # Generowanie losowych wysokości
+    distribution_z = norm(loc=0, scale=200)
+    z = distribution_z.rvs(size=num_points)
+
+    # Generowanie losowych kątów
+    distribution_theta = norm(loc=0, scale=360)
+    theta = distribution_theta.rvs(size=num_points)
+
+    # Konwersja do współrzędnych kartezjańskich
+    x = r * np.cos(theta)
+    y = r * np.sin(theta)
+
+    points = zip(x,y,z) #tworzy listę krotek x,y,z
+    return points
+
+
+cloud_points_A = generate_points_A(2000)
+with open('podpunktA.xyz', 'w', encoding='utf-8', newline='\n') as csvfile:
     csvwriter = writer(csvfile)
     # csvwriter.writerow('x','y','z') nagłówek
-    for p in cloud_points:
+    for p in cloud_points_A:
         csvwriter.writerow(p)
 
+cloud_points_B = generate_points_B(2000)
+with open('podpunktB.xyz', 'w', encoding='utf-8', newline='\n') as csvfile:
+    csvwriter = writer(csvfile)
+    # csvwriter.writerow('x','y','z') nagłówek
+    for p in cloud_points_B:
+        csvwriter.writerow(p)
+
+
+cloud_points_C = generate_points_C(2000,75)
+with open('podpunktC.xyz', 'w', encoding='utf-8', newline='\n') as csvfile:
+    csvwriter = writer(csvfile)
+    # csvwriter.writerow('x','y','z') nagłówek
+    for p in cloud_points_C:
+         csvwriter.writerow(p)
